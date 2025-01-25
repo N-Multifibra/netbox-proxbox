1f5c84cdb4c58bfe4309ca4fbe884e96b4161388
Emerson Felipe <emersonfelipe.2003@gmail.com>
2025-01-25 19:49:39 +0000

    Start using rich package to format my CLI outputs

--- proxbox_api/proxbox-cli.py
+++ proxbox_api/proxbox-cli.py
@@ -4,7 +4,13 @@ from proxbox_api.database import NetBoxEndpoint, engine
 from sqlmodel import select, Session
 import requests
 
+from rich import print
+from rich.console import Console
+from rich.table import Table
+
+
 app = typer.Typer()
+console = Console()
 
 def get_netbox() -> NetBoxEndpoint:
     with Session(engine) as session:
@@ -14,7 +20,7 @@ def get_netbox() -> NetBoxEndpoint:
         return netbox_endpoints[0]
     
 @app.command()
-def netbox(json: bool = False):
+def netbox(json: bool = False,):
     nb = get_netbox()
     
     if json:
@@ -25,7 +31,7 @@ def netbox(json: bool = False):
                 
 
 @app.command()
-def proxmox(json: bool = False):
+def proxmox(json: bool = False, table: bool = False):
     nb = get_netbox()
     try:
         url: str = f'http://{nb.ip_address}:{nb.port}/api/plugins/proxbox/endpoints/proxmox'
@@ -33,15 +39,27 @@ def proxmox(json: bool = False):
         proxmox.raise_for_status()
         proxmox = proxmox.json()
         
+        endpoints: list = proxmox.get('results')
+        
         if json:
             print(proxmox)
         
+        elif table:
+            table = Table('Name', 'IP Address', 'Port', title='Proxmox Endpoints', show_header=True)
+            for endpoint in proxmox['results']:
+                name: str = endpoint.get('name')
+                ip_address: str = endpoint['ip_address']['address'].split('/')[0]
+                port: str = str(endpoint['port'])
+            
+                table.add_row(name, ip_address, port)
+            
+            console.print(table)
+        
         else:
-            endpoints: list = proxmox.get('results')
-            print(f'[Proxmox Endpoints]')
+            print(f'[bold red][Proxmox Endpoints][/bold red]')
             for endpoint in endpoints:
                 name = endpoint.get('name')
-                ip_address = endpoint['ip_address']['address']
+                ip_address = endpoint['ip_address']['address'].split('/')[0]
                 port = endpoint['port']
                 print(f'{name} ({ip_address}:{port})')
                 
